c8281c4effe53ffb68019d43d8224c111689c046
rilieo <rilesdou@gmail.com>
2024-11-01 17:39:07 -0400

    add rearranged photos to user schema

--- db.js
+++ db.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose'
 const userSchema = new mongoose.Schema({
     username: {type: String, required: true},
     password: {type: String, required: true},
-    photos: [{type: Array, required: false}]
+    photos: [{type: Array, required: false}],
+    rearrangedPhotos: [{type: Array, required: false}]
 })
 
 const User = mongoose.model('User', userSchema);
