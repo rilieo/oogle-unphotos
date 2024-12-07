e4a9fddc85da19e85a77049d42d45c05f09bfa83
rilieo <rilesdou@gmail.com>
2024-12-06 20:56:48 -0500

    add cors

--- app/backend/server.js
+++ app/backend/server.js
@@ -1,20 +1,19 @@
 import { config } from 'dotenv';
 config();
+import { albumRouter } from './routes/albums.js';
 import { authRouter } from './routes/auth.js';
 import { photosRouter } from './routes/photos.js';
+import cors from 'cors';
 import express from 'express'
 import mongoose from 'mongoose';
-import path from 'path'
-import { fileURLToPath } from 'url';
 
 mongoose.connect(process.env.DSN)
 
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT ?? 3001;
 
 app.use(express.json({ limit: '10mb' }));
+app.use(cors());
 app.use('/api/auth', authRouter);
 app.use('/api/photos', photosRouter);
 
