7f896545b7ee38a5cccc3aa16683c03336997092
rilieo <rilesdou@gmail.com>
2024-12-07 22:59:32 -0500

    add cors

--- app/backend/server.js
+++ app/backend/server.js
@@ -2,6 +2,7 @@ import { config } from 'dotenv';
 config();
 import { authRouter } from './routes/auth.js';
 import { photosRouter } from './routes/photos.js';
+import cors from 'cors';
 import express from 'express'
 import mongoose from 'mongoose';
 
@@ -10,6 +11,7 @@ mongoose.connect(process.env.DSN);
 const app = express();
 const PORT = process.env.PORT ?? 3001;
 
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use('/api/auth', authRouter);
 app.use('/api/photos', photosRouter);
