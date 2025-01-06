c1339c0d8772af6d7ac61653ca3cb0c57b2fd7c0
rilieo <rilesdou@gmail.com>
2025-01-05 22:07:36 -0600

    add a beautiful image to homepage

--- app/frontend/src/assets/index.js
+++ app/frontend/src/assets/index.js
@@ -1,7 +1,9 @@
 import back from './icons/back.png';
 import upload from './icons/upload.png';
+import rando from './rando.png';
 
 export {
     back,
-    upload
+    upload,
+    rando
 };
--- app/frontend/src/pages/Home.jsx
+++ app/frontend/src/pages/Home.jsx
@@ -1,9 +1,17 @@
+import { rando } from '../assets';
 import Navbar from '../components/Navbar';
 
 const Home = () => {
   return (
     <div className="flex flex-col items-center">
       <Navbar />
+      <img src={rando} 
+        alt="cover image" 
+        className="object-contain w-[400px] h-[400px] my-20"
+      />
+      <h1 className="text-3xl font-bold">Where your photos are jumbled...</h1>
+      <br />
+      <h2 className="text-2xl">just like life</h2>
     </div>
   );
 };
