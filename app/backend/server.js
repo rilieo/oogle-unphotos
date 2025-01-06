f622f2e1ec3b3e4c597902ae894ad1ac359b3856
rilieo <rilesdou@gmail.com>
2025-01-05 19:48:13 -0600

    add cors options

--- app/backend/server.js
+++ app/backend/server.js
@@ -10,7 +10,11 @@ mongoose.connect(process.env.DSN);
 const app = express();
 const PORT = process.env.BACKEND_PORT ?? 3001;
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://oogle-unphotos-j55a.vercel.app',
+    methods: ['GET', 'POST'],
+    credentials: true,
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use('/api/auth', authRouter);
 app.use('/api/photos', photosRouter);
