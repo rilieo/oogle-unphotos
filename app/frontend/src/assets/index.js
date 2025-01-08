d8dc79dd9732e91bcc7c71a070d91b50469898bc
rilieo <rilesdou@gmail.com>
2025-01-07 22:49:13 -0600

    add feature to show/hide passwords

--- app/frontend/src/assets/index.js
+++ app/frontend/src/assets/index.js
@@ -1,9 +1,13 @@
 import back from './icons/back.png';
+import eye from './icons/eye.png';
+import eyehide from './icons/eye-hide.png';
 import upload from './icons/upload.png';
 import rando from './rando.png';
 
 export {
     back,
+    eye,
+    eyehide,
     upload,
     rando
 };
--- app/frontend/src/components/Form.jsx
+++ app/frontend/src/components/Form.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { postData } from '../lib/db';
+import { eye, eyehide } from '../assets';
 import toast from 'react-hot-toast';
 
 const Form = ({ route, successMsg, value }) => {
@@ -9,6 +10,7 @@ const Form = ({ route, successMsg, value }) => {
     password: ''
   });
   const { setUser } = useAuth();
+  const [type, setType] = useState('password');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -30,10 +32,20 @@ const Form = ({ route, successMsg, value }) => {
     localStorage.setItem('user', JSON.stringify(data));
   };
 
+  const toggleVisibility = (e) => {
+    e.preventDefault();
+    setType(type === 'text' ? 'password' : 'text');
+  };
+
   return (
     <form className="flex flex-col mt-10" onSubmit={handleSubmit}>
-      <input type="text" name="username" placeholder="Username" className="h-[50px] border-b-2 p-2" onChange={(e) => setUserInfo({...userInfo, username: e.target.value})}/>
-      <input type="password" name="password" placeholder="Password" className="h-[50px] border-b-2 p-2" onChange={(e) => setUserInfo({...userInfo, password: e.target.value})}/>
+      <input type="text" name="username" placeholder="Username" className="h-[50px] w-full border-b-2 p-2" onChange={(e) => setUserInfo({...userInfo, username: e.target.value})}/>
+      <div className="relative">
+        <button onClick={toggleVisibility} className="absolute top-3 right-1">
+          <img src={type === 'password' ? eye : eyehide} className="w-6 h-6"/>
+        </button>
+        <input type={type} name="password" placeholder="Password" className="h-[50px] w-full border-b-2 p-2" onChange={(e) => setUserInfo({...userInfo, password: e.target.value})}/>
+      </div>
       <input type="submit" value={value} className="h-[50px] bottom-2 bg-primary text-white font-bold cursor-pointer w-full mt-10 hover:bg-blue-600"/>
     </form>
   );
